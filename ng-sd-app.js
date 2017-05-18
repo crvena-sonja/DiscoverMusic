b3899245903c0d2cf7e6577642d3631b6c94b067
PROB8 <blackb8r@gmail.com>
2017-05-18 18:31:35 -0400

    state mod functions

--- ng-sd-app.js
+++ ng-sd-app.js
@@ -1,56 +1,52 @@
 
 
-const myKey = "AIzaSyAoZTIZrg64L6UnWUI-qj8sTE8WhPF7VRQ";
-const base_nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-const base_details_url = "https://maps.googleapis.com/maps/api/place/details/json";
-
 const appState = {
 
 	results: [],
+	artistID: [],
+	artistAlbumID: [],
+	artistTracks: []
 }
 
 
-function queryGoogleMaps (myAddress){
-	// const query = {										
- //        key: myKey,
- //        location: myAddress,
- //        radius: 32186.88, //20 miles in meters
- //        rankby: "prominence",
- //        type: 'spa',
-	// }
+function querySpotifyArtist (){
 
 	const query1 = {
-		type: 'artist',
-		q: "Beyonce"
+		type: 'artist', 														
+		q: "beyonce"
+
 	}
 
-	$.getJSON("https://api.spotify.com/v1/search", query1, (response) => {
-			console.log(response);
-		});
+	$.getJSON("https://api.spotify.com/v1/search", query1, function(response)  {
+			 appState.artistID.push(response.artists.items[0].id);
+				querySpotifyAlbums();
 
-	const query2 = {
-		type: 'artist',
-		q: "Childish%20Gambino"
-	}
+	});
 
-	$.getJSON("https://api.spotify.com/v1/search", query2, (response) => {
-			console.log(response);
-		});
-
-	// $.ajax({
-	// 	 	url: "https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE", 
-	// 	 	type: "GET",  
-	// 	 	cache: false, 
- //           	dataType: 'jsonp',
- //            success: function(response){                          
- //                console.log(response);                   
- //            }           
-	// });
 } 
 
-queryGoogleMaps();
+function querySpotifyAlbums(){
+		id: appState.artistID[0]
+	}
 
+	$.getJSON(`https://api.spotify.com/v1/artists/${appState.artistID[0]}/albums`, query2, (response) => {				//query2 GETS US THE TRACKS USING ARTIST ID
+						response.items.forEach( item => appState.artistAlbumID.push(item.id))
+						
 
-// https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyAoZTIZrg64L6UnWUI-qj8sTE8WhPF7VRQ
+	});
+}
 
-// https://maps.googleapis.com/maps/api/place/nearbysearch/?output=json&key=AIzaSyAoZTIZrg64L6UnWUI-qj8sTE8WhPF7VRQ&location=39.065718%2C%20-77.503350&radius=32186.88&rankby=prominence&type=spa
+function querySpotifyTracks(){
+	$.getJSON(``)               /////do a .join(',')
+}
+
+querySpotifyArtist();
+
+// console.log('bam', appState.artistID);
+
+//1 get artist
+//2 get id
+//3 get albums
+//4 get songs
+
+//https://api.spotify.com/v1/artists/{id}/albums
