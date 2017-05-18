072a821116840e97b042ef566640f47f2ee74765
Sonja Duric <duric.sonja@gmail.com>
2017-05-18 14:34:00 -0400

    wireframes api base urls added

---
+++ ng-sd-app.js
@@ -0,0 +1,4 @@
+
+const key = "AIzaSyAoZTIZrg64L6UnWUI-qj8sTE8WhPF7VRQ";
+const base_nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+const base_details_url = "https://maps.googleapis.com/maps/api/place/details/json";
