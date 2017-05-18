e813bcb2516d11c64c7075dd7bb6827368f5cd52
PROB8 <blackb8r@gmail.com>
2017-05-18 13:38:58 -0400

    appState addition

---
+++ ng-sd-app.js
@@ -0,0 +1,14 @@
+const appState = {
+
+	results: []
+}
+
+const baseURL = ;
+
+function queryGoogleMaps (searchDirection){
+	let query = {
+		q: searchDirection,											
+        key: 'AIzaSyBsVbwSx7O3-lG0IyK2nsd2qCr28MUcqrw',
+        part: 'snippet'
+	}
+} 
