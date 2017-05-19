893effa274771582de82e33531bfadfb257557fd
Sonja Duric <duric.sonja@gmail.com>
2017-05-19 00:51:33 -0400

    search artist and display 10 titles

--- ng-sd-app.js
+++ ng-sd-app.js
@@ -2,45 +2,103 @@
 
 const appState = {
 
-	results: [],
+	search: "",
 	artistID: [],
 	artistAlbumID: [],
-	artistTracks: []
+	availAlbums: [],
+	initialTracks: [],
+	trackIDs: [],
+	lowPopTracks: []
 }
 
 
-function querySpotifyArtist (){
+function querySpotifyArtist (search){
 
 	const query1 = {
 		type: 'artist', 														
-		q: "beyonce"
-
+		q: search
 	}
-
 	$.getJSON("https://api.spotify.com/v1/search", query1, function(response)  {
 			 appState.artistID.push(response.artists.items[0].id);
 				querySpotifyAlbums();
-
 	});
 
 } 
 
 function querySpotifyAlbums(){
-		id: appState.artistID[0]
+	$.getJSON(`https://api.spotify.com/v1/artists/${appState.artistID[0]}/albums`, {}, (response) => {				//query2 GETS US THE TRACKS USING ARTIST ID
+						response.items.forEach( item => appState.artistAlbumID.push(item.id));
+						querySpotifyTrackIDs();
+	});
+}
+
+function querySpotifyTrackIDs(){
+	const query3 = {
+		ids: appState.artistAlbumID.join(','),
 	}
+	console.log(query3.ids);
 
-	$.getJSON(`https://api.spotify.com/v1/artists/${appState.artistID[0]}/albums`, query2, (response) => {				//query2 GETS US THE TRACKS USING ARTIST ID
-						response.items.forEach( item => appState.artistAlbumID.push(item.id))
-						
+	$.getJSON(`https://api.spotify.com/v1/albums`, query3, (response) => { 
+		console.log(response);
 
+		appState.availAlbums.push(response.albums.filter( function(element) {
+			return (element.available_markets.includes("US"));
+			}));
+
+		//console.log(appState.availAlbums);
+		appState.availAlbums[0].forEach(element2 => { element2.tracks.items.forEach(
+			element => {appState.initialTracks.push(element);} )} );
+		//console.log(appState.initialTracks);
+		appState.initialTracks.forEach(element => appState.trackIDs.push(element.id));
+		console.log(appState.trackIDs);
+		querySpotifyTracks();
 	});
 }
 
 function querySpotifyTracks(){
-	$.getJSON(``)               /////do a .join(',')
+	const query4 = {
+		ids: appState.trackIDs.slice(0, 50).join(','),
+	}
+
+	$.getJSON('https://api.spotify.com/v1/tracks', query4, (response) => {
+		(response.tracks.filter(element => element.popularity <= 50)).forEach(element => appState.lowPopTracks.push(element));
+		console.log(appState.lowPopTracks);
+		renderTracks($('.tracks'));
+
+	});
 }
 
-querySpotifyArtist();
+function renderTracks(element){
+	let html = `<ul>`;
+
+	let j;
+	appState.lowPopTracks.length >= 10 ? j=10 : j=appState.lowPopTracks.length;
+
+	for(let i = 0; i < j; i++){
+		html += `<li>${appState.lowPopTracks[i].name}</li>`;
+	}
+
+	html += `</ul>`;
+	element.html(html);
+	element.removeClass("hidden");
+}
+
+function addListeners(){
+
+	$('form').on('submit', function(event){
+		event.preventDefault();
+		appState.search = $('#search-spotify').val();
+		querySpotifyArtist(appState.search);
+	});
+}
+
+$(function () {
+
+addListeners();
+
+});
+
+//querySpotifyArtist();
 
 // console.log('bam', appState.artistID);
 
