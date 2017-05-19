1577158bab88017c3e9cf19c6e3c974f20c566d3
PROB8 <blackb8r@gmail.com>
2017-05-18 23:02:53 -0400

    buggy code/url encoding

--- ng-sd-app.js
+++ ng-sd-app.js
@@ -5,19 +5,20 @@ const appState = {
 	results: [],
 	artistID: [],
 	artistAlbumID: [],
-	artistTracks: []
+	artistTracks: [],
+	albumsIDs: []
 }
 
+// console.log('that', appState.albumsIDs);
+function querySpotifyArtist (artist){
 
-function querySpotifyArtist (){
-
-	const query1 = {
+	const query = {
 		type: 'artist', 														
-		q: "beyonce"
+		q: artist
 
 	}
 
-	$.getJSON("https://api.spotify.com/v1/search", query1, function(response)  {
+	$.getJSON("https://api.spotify.com/v1/search", query, function(response)  {
 			 appState.artistID.push(response.artists.items[0].id);
 				querySpotifyAlbums();
 
@@ -26,27 +27,61 @@ function querySpotifyArtist (){
 } 
 
 function querySpotifyAlbums(){
+									///STORE RESULT IN A VARIABLE TO BE USED LATER
+	const query = {
 		id: appState.artistID[0]
 	}
 
-	$.getJSON(`https://api.spotify.com/v1/artists/${appState.artistID[0]}/albums`, query2, (response) => {				//query2 GETS US THE TRACKS USING ARTIST ID
+	$.getJSON(`https://api.spotify.com/v1/artists/${appState.artistID[0]}/albums`, query, (response) => {				//query2 GETS US THE TRACKS USING ARTIST ID
 						response.items.forEach( item => appState.artistAlbumID.push(item.id))
-						
-
+				// console.log(appState.artistAlbumID);	
+				 querySpotifyTracks();	
+// 
 	});
 }
 
-function querySpotifyTracks(){
-	$.getJSON(``)               /////do a .join(',')
-}
+ 	function querySpotifyTracks(){
+ 	 albumIDs = appState.artistAlbumID.join(",");
+ 		console.log(albumIDs);
+ 		// const query = {
 
-querySpotifyArtist();
+ 		// 	id: albumIDs
+
+ 		// }
+
+
+
+ 	// 	$.getJSON(`https://api.spotify.com/v1/albums`, query, (response) => {				//query2 GETS US THE TRACKS USING ARTIST ID
+		// 				// response.items.forEach( item => appState.artistAlbumID.push(item.id))
+		// 		console.log(response);	
+		// })
+
+
+
+		$.ajax({
+    type: "POST",
+    url: "https://api.spotify.com/v1/albums",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    data: "{id: '" + albumIDs + "'}",
+    success: function(json) {
+        $("#success").html("json.length=" + json.length);
+        itemAddCallback(json);
+    },
+    // error: function (xhr, textStatus, errorThrown) {
+    //     $("#error").html(xhr.responseText);
+    // }
+});
+	}
+
+querySpotifyArtist('adele');
 
 // console.log('bam', appState.artistID);
 
 //1 get artist
 //2 get id
 //3 get albums
+
 //4 get songs
 
 //https://api.spotify.com/v1/artists/{id}/albums
